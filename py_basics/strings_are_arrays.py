f4550ee113b77ac79369c21577fe505bff2d63d5
Yash Lad <yashlad727@gmail.com>
2022-08-09 19:51:26 +0530

    check and if

--- py_basics/strings_are_arrays.py
+++ py_basics/strings_are_arrays.py
@@ -3,3 +3,16 @@ print(a[1])
 
 for x in a:
     print(x)
+
+b = "hello india"
+print(len(b))
+
+# check string
+txt = "The besy things in life are free"
+print("free" in txt)
+
+print("Expensive" not in txt)
+
+# using an if statement:
+if "expesive" not in txt:
+    print("No, 'expensive' is NOT present")
